789d76085e5303d57042ab2d3a6527381ade18eb
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-12-02 16:14:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     videoContainer.style.display = "block";
 
     // Установка видео (замените ссылки на ваши видеофайлы)
-    mainVideo.src = "https://www.w3schools.com/html/mov_bbb.mp4";
+    mainVideo.src = "https://goods-vod.kwcdn.com/goods-video/18e526c8d03123f14de2cbd14066eb53e487e856.f30.mp4";
     cornerVideo.src = "https://github.dev/UzunDemir/jucarii/blob/main/Untitled%20video(1).mp4";
   }, 3000); // 3 секунды
 });
