2944dcdc7e69d6cf4522124883abaee428ad412e
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-12-02 16:19:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const mainVideo = document.getElementById("main-video");
   const cornerVideo = document.getElementById("corner-video");
 
-  // Эмуляция звонка
   setTimeout(() => {
     callIcon.style.display = "none";
     videoContainer.style.display = "block";
 
-    // Установка видео (замените ссылки на ваши видеофайлы)
+    // Установка видео
     mainVideo.src = "https://goods-vod.kwcdn.com/goods-video/18e526c8d03123f14de2cbd14066eb53e487e856.f30.mp4";
-    cornerVideo.src = "https://github.dev/UzunDemir/jucarii/blob/main/Untitled%20video(1).mp4";
-  }, 3000); // 3 секунды
+    cornerVideo.src = "https://github.dev/UzunDemir/jucarii/blob/main/Untitled%20video(1).mp4"";
+
+    mainVideo.play().catch((err) => console.error("Ошибка воспроизведения основного видео:", err));
+    cornerVideo.play().catch((err) => console.error("Ошибка воспроизведения углового видео:", err));
+  }, 3000);
 });
